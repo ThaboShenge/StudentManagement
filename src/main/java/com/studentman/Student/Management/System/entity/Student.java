3d0f66b5781863c00b8699ed7e0a427514ee4dde
Thabo Buthelezi <thabo.buthelezi.tb94@gmail.com>
2023-08-21 15:52:53 +0200

    first commit

---
+++ src/main/java/com/studentman/Student/Management/System/entity/Student.java
@@ -0,0 +1,9 @@
+package com.studentman.Student.Management.System.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "students")
+public class Student {
+}
