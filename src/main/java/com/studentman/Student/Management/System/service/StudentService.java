84cbee85138c942faa5ebf72f227644ff6786ccb
Thabo Buthelezi <thabo.buthelezi.tb94@gmail.com>
2023-08-22 17:03:01 +0200

    Added controller and service

---
+++ src/main/java/com/studentman/Student/Management/System/service/StudentService.java
@@ -0,0 +1,18 @@
+package com.studentman.Student.Management.System.service;
+
+import com.studentman.Student.Management.System.entity.Student;
+
+import java.util.List;
+
+public interface StudentService {
+
+    List<Student> getAllStudents();
+
+    Student saveStudent(Student student);
+
+    Student getStudentById(Long id);
+
+    Student updateStudent(Student student);
+
+    void deleteStudentById(Long id);
+}
---
+++ src/main/java/com/studentman/Student/Management/System/service/impl/StudentServiceImpl.java
@@ -0,0 +1,41 @@
+package com.studentman.Student.Management.System.service.impl;
+
+import com.studentman.Student.Management.System.entity.Student;
+import com.studentman.Student.Management.System.service.StudentService;
+
+import java.util.List;
+
+public class StudentServiceImpl implements StudentService {
+
+    private StudentRepository studentRepository;
+
+    public StudentServiceImpl(StudentRepository studentRepository) {
+        super();
+        this.studentRepository = studentRepository;
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    @Override
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    @Override
+    public Student getStudentById(Long id) {
+        return studentRepository.findById(id).get();
+    }
+
+    @Override
+    public Student updateStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    @Override
+    public void deleteStudentById(Long id) {
+        studentRepository.deleteById(id);
+    }
+}
