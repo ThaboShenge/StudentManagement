9b567ddc28eebc91169bca450ae198567bee2eea
Thabo Buthelezi <thabo.buthelezi.tb94@gmail.com>
2023-08-23 16:21:50 +0200

    fixed few errors

--- src/main/java/com/studentman/Student/Management/System/repository/StudentRepository.java
+++ src/main/java/com/studentman/Student/Management/System/repository/StudentRepository.java
@@ -1,5 +1,12 @@
 package com.studentman.Student.Management.System.repository;
 
+import com.studentman.Student.Management.System.entity.Student;
+
+import java.util.List;
+
 public interface StudentRepository {
 
+    List<Student> getAllStudents();
+
+    Student saveStudent(Student student);
 }
