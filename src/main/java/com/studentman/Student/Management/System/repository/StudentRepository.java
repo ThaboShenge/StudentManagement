e014b5a60b51fb8d9066e2ea165458e48d23a1bd
Thabo Buthelezi <thabo.buthelezi.tb94@gmail.com>
2023-08-23 16:29:05 +0200

    repo and service working

--- src/main/java/com/studentman/Student/Management/System/repository/StudentRepository.java
+++ src/main/java/com/studentman/Student/Management/System/repository/StudentRepository.java
@@ -1,12 +1,21 @@
 package com.studentman.Student.Management.System.repository;
 
 import com.studentman.Student.Management.System.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-public interface StudentRepository {
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
 
     List<Student> getAllStudents();
 
     Student saveStudent(Student student);
+
+    Student getStudentById(Long id);
+
+    Student updateStudent(Student student);
+
+    void deleteStudentById(Long id);
 }
